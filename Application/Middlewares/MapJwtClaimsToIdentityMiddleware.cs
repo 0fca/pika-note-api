db7f1e150304e22bd101ca8f986f84d44c66a04d
Ofca <lukasz.bownik@sidnet.pl>
2026-03-15 21:53:57 +0100

    Fixes to make audience being ignored to not match it to Oiddict handler

--- Application/Middlewares/MapJwtClaimsToIdentityMiddleware.cs
+++ Application/Middlewares/MapJwtClaimsToIdentityMiddleware.cs
@@ -1,12 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
 using System.Security.Claims;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
-using OpenIddict.Abstractions;
-using Pika.Domain.Security;
 using PikaNoteAPI.Application.Extensions.Enums;
 
 namespace PikaNoteAPI.Application.Middlewares;
@@ -25,25 +22,31 @@ public class MapJwtClaimsToIdentityMiddleware
         var token = context.Request.Cookies[".AspNet.Identity"];
         if (string.IsNullOrEmpty(token))
         {
-            context.User.AddClaim(ClaimTypes.Role, RoleString.User);
             await _next(context);
             return;
         }
         var handler = new JwtSecurityTokenHandler();
         var jsonToken = handler.ReadToken(token);
         var jwst = jsonToken as JwtSecurityToken;
-        jwst!.Claims.ToList().ForEach(c =>
+
+        var identity = new ClaimsIdentity("PikaCore");
+        foreach (var claim in jwst!.Claims)
         {
-            if (c.Type is not (KeycloakClaimTypes.RealmAccess)) return;
-            var roleClaimTypeName = context.User.Identities.First().RoleClaimType;
-            const string roleClaimStandardName = ClaimTypes.Role;
-            var roles = JsonSerializer.Deserialize<Dictionary<string, IList<string>>>(c.Value)["roles"];
-            foreach (var role in roles)
+            if (claim.Type is KeycloakClaimTypes.RealmAccess)
             {
-                context.User.AddClaim(roleClaimTypeName, role);
-                context.User.AddClaim(roleClaimStandardName, role);
+                var roles = JsonSerializer.Deserialize<Dictionary<string, IList<string>>>(claim.Value)!["roles"];
+                foreach (var role in roles)
+                {
+                    identity.AddClaim(new Claim(ClaimTypes.Role, role));
+                }
             }
-        });
+            else
+            {
+                identity.AddClaim(new Claim(claim.Type, claim.Value, claim.ValueType));
+            }
+        }
+        context.User = new ClaimsPrincipal(identity);
+
         await _next(context);
     }
 }
